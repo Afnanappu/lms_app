74c73cf42d0fb378aa179c2b41541b3e628ffe2f
AFNAN P <afnanp860@gmail.com>
2025-04-12 10:12:47 +0530

    Add: video list card component

---
+++ lib/components/video_list_card.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:lms_app/core/constants/app_colors.dart';
+import 'package:lms_app/models/video_model.dart';
+
+class VideoListCard extends StatelessWidget {
+  const VideoListCard({super.key, required this.videoModel, this.onTap});
+
+  final VideoModel videoModel;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: onTap,
+        title: Text(
+          videoModel.title,
+          style: TextStyle(
+            color: AppColors.accentPink,
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+        ),
+        subtitle: Text(
+          videoModel.description,
+          style: TextStyle(fontSize: 14),
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+        ),
+        leading: Icon(
+          Icons.play_circle_fill_rounded,
+          color: AppColors.accentPink,
+        ),
+      ),
+    );
+  }
+}
--- lib/views/module_details_screen.dart
+++ lib/views/module_details_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lms_app/components/video_list_card.dart';
+import 'package:lms_app/view_models/videos_provider.dart';
+import 'package:provider/provider.dart';
 
 class ModuleDetailsScreen extends StatelessWidget {
   const ModuleDetailsScreen({super.key});
@@ -6,8 +9,23 @@ class ModuleDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Module Details')),
-      body: const Center(child: Text('Module Details')),
+      appBar: AppBar(title: const Text('Videos')),
+      body: Consumer<VideosProvider>(
+        builder: (context, provider, child) {
+          return ListView.builder(
+            itemCount: provider.videoModels.length,
+            itemBuilder: (context, index) {
+              final videoModel = provider.videoModels[index];
+              return VideoListCard(
+                videoModel: videoModel,
+                onTap: () {
+                  //TODO: Navigate to video screen
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
