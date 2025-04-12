85de813980e2205a33a5f52b1ddd182b666f3b2c
AFNAN P <afnanp860@gmail.com>
2025-04-12 15:33:05 +0530

    feat: Add video player for youtube and vimeo

--- lib/components/custom_video_player.dart
+++ lib/components/custom_video_player.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:lms_app/core/constants/app_colors.dart';
 import 'package:lms_app/models/video_model.dart';
+import 'package:vimeo_player_flutter/vimeo_player_flutter.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class CustomVideoPlayer extends StatelessWidget {
   const CustomVideoPlayer({
@@ -16,25 +20,25 @@ class CustomVideoPlayer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    log("Type: $videoType URL: $url");
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Card(
           child: Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(15.0),
             child: Container(
               decoration: BoxDecoration(
                 color: AppColors.chartGray,
                 borderRadius: BorderRadius.circular(12),
               ),
-              height: 200,
-              width: size.width * 0.8,
+              // height: 200,
+              width: size.width * 0.9,
 
-              child: Stack(
-                children: [
-                  Center(child: Icon(Icons.play_circle_fill_rounded, size: 50)),
-                ],
-              ),
+              child:
+                  videoType == VideoType.youtube
+                      ? YouTubeVideoPlayer(videoUrl: url)
+                      : VimeoPlayerWidget(videoId: url.split('/').last),
             ),
           ),
         ),
@@ -43,16 +47,95 @@ class CustomVideoPlayer extends StatelessWidget {
   }
 }
 
-class PlayVideoFromNetwork extends StatefulWidget {
-  const PlayVideoFromNetwork({super.key});
+class VimeoPlayerWidget extends StatelessWidget {
+  final String videoId;
 
-  @override
-  State<PlayVideoFromNetwork> createState() => _PlayVideoFromNetworkState();
-}
+  const VimeoPlayerWidget({super.key, required this.videoId});
 
-class _PlayVideoFromNetworkState extends State<PlayVideoFromNetwork> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AspectRatio(
+      aspectRatio: 16 / 9,
+      child: VimeoPlayer(videoId: videoId),
+    );
+  }
+}
+
+class YouTubeVideoPlayer extends StatefulWidget {
+  final String videoUrl;
+
+  const YouTubeVideoPlayer({super.key, required this.videoUrl});
+
+  @override
+  State<YouTubeVideoPlayer> createState() => _YouTubeVideoPlayerState();
+}
+
+class _YouTubeVideoPlayerState extends State<YouTubeVideoPlayer> {
+  late YoutubePlayerController _controller;
+  bool isMute = false;
+
+  @override
+  void initState() {
+    super.initState();
+    final videoId = YoutubePlayer.convertUrlToId(widget.videoUrl);
+    log(videoId.toString());
+    _controller = YoutubePlayerController(
+      initialVideoId: videoId ?? "",
+      flags: YoutubePlayerFlags(
+        autoPlay: false,
+        mute: isMute,
+        controlsVisibleAtStart: true,
+        enableCaption: false,
+        hideThumbnail: false,
+        showLiveFullscreenButton: false,
+        useHybridComposition: false,
+        isLive: false,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return YoutubePlayer(
+      controller: _controller,
+      showVideoProgressIndicator: true,
+      aspectRatio: 16 / 9,
+      bottomActions: [
+        CurrentPosition(),
+        ProgressBar(
+          isExpanded: true,
+          colors: ProgressBarColors(
+            playedColor: Colors.red,
+            handleColor: Colors.redAccent,
+          ),
+        ),
+
+        PlaybackSpeedButton(),
+
+        // volumn controller
+        IconButton(
+          onPressed: () {
+            setState(() {
+              if (isMute) {
+                _controller.unMute();
+              } else {
+                _controller.mute();
+              }
+              isMute = !isMute;
+            });
+          },
+          icon: Icon(
+            !isMute ? Icons.volume_up_rounded : Icons.volume_off_rounded,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/views/video_details_screen.dart
+++ lib/views/video_details_screen.dart
@@ -10,39 +10,41 @@ class VideoDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Video Details')),
-      body: Column(
-        children: [
-          //TODO: try to add here animation
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //TODO: try to add here animation
 
-          // Video Player
-          CustomVideoPlayer(
-            url: videoModel.videoUrl,
-            videoType: videoModel.videoType,
-          ),
-
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 8,
-              children: [
-                Text(
-                  videoModel.title,
-                  style: const TextStyle(
-                    color: AppColors.primaryLightPurple,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-
-                Text(
-                  videoModel.description,
-                  style: const TextStyle(fontWeight: FontWeight.w500),
-                ),
-              ],
+            // Video Player
+            CustomVideoPlayer(
+              url: videoModel.videoUrl,
+              videoType: videoModel.videoType,
             ),
-          ),
-        ],
+
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 8,
+                children: [
+                  Text(
+                    videoModel.title,
+                    style: const TextStyle(
+                      color: AppColors.primaryLightPurple,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+
+                  Text(
+                    videoModel.description,
+                    style: const TextStyle(fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
