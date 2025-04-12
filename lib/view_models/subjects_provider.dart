096c9b3b8a25782ebc56750eb6f39f23b506677a
AFNAN P <afnanp860@gmail.com>
2025-04-12 11:07:14 +0530

    update subject provider

--- lib/view_models/subjects_provider.dart
+++ lib/view_models/subjects_provider.dart
@@ -6,13 +6,25 @@ class SubjectProvider extends ChangeNotifier {
   final ApiService _apiService;
   SubjectProvider(this._apiService);
 
-  List<SubjectModel> subjects = [];
+  List<SubjectModel> _subjects = [];
+  bool _isLoading = false;
+  String? _errorMessage;
+
+  List<SubjectModel> get subjects => _subjects;
+  bool get isLoading => _isLoading;
+  String? get errorMessage => _errorMessage;
 
   Future<void> fetchSubjects() async {
-    final data = await _apiService.fetchSubjects();
-
-    subjects = data;
-
+    _isLoading = true;
     notifyListeners();
+    try {
+      _subjects = await _apiService.fetchSubjects();
+      _errorMessage = null;
+    } catch (e) {
+      _errorMessage = e.toString();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
   }
 }
